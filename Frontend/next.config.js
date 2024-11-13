ed90904b4f96aae112cfa20c6684325ec24ec70a
Kalpit Thakur <veerkalpit@gmail.com>
2024-11-13 13:54:42 +0530

    Initial commit

---
+++ Frontend/next.config.js
@@ -0,0 +1,17 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'localhost',
+          port: '1337',
+          pathname: '/**',
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
---
+++ Frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Main from "./components/Homepage/Main";
+
+export default function Home() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
